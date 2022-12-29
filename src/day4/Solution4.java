91ffb1eb3ac9c0ca9ecd44cf050a425dc67d4ddc
지민 백 <s2104@e-mirim.hs.kr>
2022-12-29 14:20:15 +0900

    숫자 게임 다른 풀이 추가

--- src/day4/Solution4.java
+++ src/day4/Solution4.java
@@ -8,13 +8,44 @@ import java.util.stream.Collectors;
 public class Solution4 {
     public int solution(int[] A, int[] B) {
         int answer = -1;
-        //A: 첫번째 팀
+        //A: 첫번째 팀 ==> 출전 순서를 정해서 공개
         //B: 두번째 팀 ==> B팀의 승점 갯수
+        //==>A팀을 보고 B팀의 최종 승점을 가장 많이 올릴 수 있는 순서를 정함
+        //A의 i번쨰 원소가 B팀의 i번째 팀원이 부여 받은 수
 
         //[5,1,3,7]	[2,2,6,8]
-        //1 3 5 7
         //2 2 6 8
 
+        //방법1> a팀은 그대로, b팀은 정렬한 후 이중 for문으로 접근
+
+//        //A를 이길 수 있는 B 찾기 ==> 이중 for
+//        Arrays.sort(B); //오름차순 정렬
+//
+//        for(int i = 0; i<A.length; i++){
+//            for(int j = 0; j<B.length; j++){
+//                if(A[i] < B[j]){
+//                    answer++;
+//                    B[j]=0;
+//                    break;
+//                }
+//            }
+//        }
+
+
+        //방법2> a팀과 b팀을 정렬한 후 A의 뒤에서부터 반복하면서 A와 B 비교
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int index = B.length-1;
+        for(int i = A.length-1; i>=0; i--){
+            if(A[i] < B[index]){
+                index--;
+                answer++; //1357  2268
+            }
+        }
+
+
+        // 내가 푼 방법>
         Arrays.sort(A);
         Arrays.sort(B);
 

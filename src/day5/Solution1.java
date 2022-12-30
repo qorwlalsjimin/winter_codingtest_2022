9f924be381c1898489a066bacb79e8bc8776586d
지민 백 <s2104@e-mirim.hs.kr>
2022-12-30 10:14:34 +0900

    해시테이블 알고리즘을 이용한 문제 풀이

--- src/day5/Solution1.java
+++ src/day5/Solution1.java
@@ -1,6 +1,8 @@
 package day5;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //전화번호 목록 https://school.programmers.co.kr/learn/courses/30/lessons/42577
 public class Solution1 {
@@ -18,6 +20,22 @@ public class Solution1 {
 //            if(phone_book[i+1].substring(0, phone_book[i].length()).equals(phone_book[i]))
             if(phone_book[i+1].startsWith((phone_book[i])))
                 return false;
+        }
+
+        //방법2> HashMap 이용
+        //1> Map 만들기: 전화번호값(key), value : 전화번호의 인덱스
+        HashMap<String, Integer> map = new HashMap<>(phone_book.length);
+
+        //2> Map에 값 넣기
+        for(String phone : phone_book)
+            map.put(phone, 1);
+
+        //3> 전화번호의 값이 map에 존재하는지 확인
+        for(String phone : phone_book) {
+            for (int i = 0; i < phone.length(); i++){
+                if(map.get( phone.substring(0,i) )==Integer.valueOf(1))
+                    return false;
+            }
         }
         return answer;
     }

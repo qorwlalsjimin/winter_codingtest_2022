5018036739901f8c04d8f043eca0e4d5b1ee2117
지민 백 <s2104@e-mirim.hs.kr>
2023-01-03 12:35:53 +0900

    구명보트 문제 주석 추가

--- src/day3/quest/Solution1.java
+++ src/day3/quest/Solution1.java
@@ -17,15 +17,28 @@ public class Solution1 {
 
         while(first<=last) {
             if (people[first] + people[last] > limit) { //두 사람이 같이 못탈때
-                last--;
-                answer++;
+                last--; //한사람만 태워주고
+                answer++; //배를 보낸다
             }else{
                 //두 사람이 같이 탈 수 있을때
-                first++;
-                last--;
-                answer++;
+                first++; //몸무게가 적은 사람도 태워주고
+                last--; //몸무게가 많은 사람도 태워준다
+                answer++; //배를 보낸다
             }
         }
+
+        //--★문제 풀이 이해하는 tip!★--
+        //--first나 last가 움직이면 누군가가 배를 탔다는 의미이다
+        //--아래와 같은 상황을 살펴보자
+        //--
+        //--0 1 2(first) 3 4 5(last) 6 7
+        //--위와 같은 상황은 while문이 반복되고 있는 상황 중 하나이다
+        //--숫자는 몸무게를 뜻하는 것이 아닌 index 번호를 뜻한다
+        //--
+        //--first는 2에 멈춰있고
+        //--last는 5에 멈춰있다
+        //--저 상황에서 0, 1, 6, 7은 이미 배를 탔고
+        //--first와 last안에 있는 2, 3, 4, 5는 아직 배를 타지 못한 상황이다
 
 
 //        //*선생님 풀이
